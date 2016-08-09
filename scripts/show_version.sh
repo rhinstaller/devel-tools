679aced8c3f82aba6efac3b1cefd57eb9c50ef5a
Jiri Konecny <jkonecny@redhat.com>
2016-08-09 16:33:53 +0200

    Add small script for getting an anaconda version

---
+++ scripts/show_version.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Set url of your repository server
+BASE_URL="http://server.example.com"
+
+
+PACKAGES_URL="rawhide/Packages/a"
+
+VERSION_ONLY=false
+
+for i in $@
+do
+    case $i in
+        -m)
+         shift
+         ;;
+        -rh7)
+         PACKAGES_URL="rhel7/Packages"
+         shift
+         ;;
+        -f23)
+         PACKAGES_URL="fedora23/Packages/a"
+         shift
+         ;;
+        -f24)
+         PACKAGES_URL="fedora24/Packages/a"
+         shift
+         ;;
+        -p)
+         VERSION_ONLY=true
+         shift
+         ;;
+    esac
+done
+
+if [[ $VERSION_ONLY = true ]]; then
+    wget -qO- "$BASE_URL/$PACKAGES_URL" | grep -o -e "\"anaconda-.*.rpm\"" | sed "s/\"//g" | head -n 1 | sed 's/anaconda-\([0-9.]*\)-[0-9]*.*/\1/'
+else
+    echo "checking anaconda version: $PACKAGES_URL"
+    wget -qO- "$BASE_URL/$PACKAGES_URL" | grep -o -e "\"anaconda-.*.rpm\"" | sed "s/\"//g" | head -n 1
+fi
