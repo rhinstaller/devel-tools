7cec492725cebcac559590549d72c36eeceb3659
Jiri Konecny <jkonecny@redhat.com>
2016-08-09 15:25:04 +0200

    Move MasterBranch and GlobalSettings to their own files

---
+++ anaconda_updates/master.py
@@ -0,0 +1,8 @@
+from . import GeneralBranch, Branch
+
+class MasterBranch(GeneralBranch):
+
+    def __init__(self):
+        super().__init__(branch_type=Branch.master,
+                         cmd_args=["-m", "--master"], help="working on Rawhide",
+                         version_script_params=["-m", "-p"])
