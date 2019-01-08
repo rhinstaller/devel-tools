ce53ba64d7548676a438eeec1394e4e3a3da92e6
Jiri Konecny <jkonecny@redhat.com>
2019-01-08 15:53:22 +0100

    Add rhel 7.6 support

---
+++ anaconda_updates/releases/rhel7_6.py
@@ -0,0 +1,17 @@
+
+from . import GeneralBranch, Branch
+from anaconda_updates.settings import GlobalSettings
+
+
+class Rhel7_6Branch(GeneralBranch):
+
+    def __init__(self):
+        super().__init__(branch_type=Branch.rhel7_6,
+                         cmd_args=["-rh7.6", "--rhel7.6"],
+                         help="working on RHEL 7.6",
+                         version="21.48.22.147",
+                         img_name="rhel7.6_updates.img",
+                         blivet_args=[], pykickstart_args=[])
+
+        GlobalSettings.use_blivet = False
+        GlobalSettings.use_pykickstart = False
