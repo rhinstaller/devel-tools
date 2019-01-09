1798b716d8d9b7cb9d1dcd2128278b37988341dc
Jiri Konecny <jkonecny@redhat.com>
2019-01-09 10:48:29 +0100

    Add first version of the script
    It's working without problems even now.

---
+++ anaconda_taimer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -eq 1 ]; then
+    HOST="$1"
+else
+    echo "Missing parameter!" >&2
+    echo "" >&2
+    echo "$0 <host_address>" >&2
+    echo "" >&2
+    echo "<host_address> - Address of the VM where Anaconda is running!" >&2
+    exit 1
+fi
+
+
+ssh -t -o StrictHostKeyChecking=no "root@$HOST" "tmux attach-session -d -t anaconda"
