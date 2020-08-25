03db5609558d4a81a1141f1ef4327e71cefd9322
Jiri Konecny <jkonecny@redhat.com>
2020-08-25 14:43:22 +0200

    Set static version for F32
    GA is long time out so the version can be specified.

--- anaconda_updates/anaconda_updates/releases/f32.py
+++ anaconda_updates/anaconda_updates/releases/f32.py
@@ -7,4 +7,4 @@ class Fedora32Branch(GeneralBranch):
         super().__init__(branch_type=Branch.fedora32,
                          cmd_args=["-f32", "--fedora32"],
                          help="working on Fedora 32",
-                         version_script_params=["-f32", "-p"])
+                         version="32.24.7")
