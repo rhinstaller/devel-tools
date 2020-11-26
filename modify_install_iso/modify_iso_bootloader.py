02c5032352567e3a0dfaced0cee7fa3438ec5f58
Jiri Konecny <jkonecny@redhat.com>
2020-11-26 17:45:23 +0100

    Add support to inject initrd overlay to installation image
    This will take your initrd overlay and inject that to the source boot.iso with
    modified isolinux.cfg so it will be automatically used in the output ISO during
    boot.
    
    This should make our testing of initrd on boot.iso much much saner in the
    future.

--- modify_install_iso/modify_iso_bootloader.py
+++ modify_install_iso/modify_iso_bootloader.py
@@ -16,6 +16,10 @@ from tempfile import TemporaryDirectory
 from lib import mount_iso, create_custom_dvd
 
 
+ISOLINUX_PATH = "isolinux/isolinux.cfg"
+INITRD_NAME = "overlay.img"
+
+
 def parse_args():
     """Parse arguments for this tool."""
     parser = ArgumentParser(description="Tool to create testing DVD with multiple repositories.")
@@ -31,11 +35,17 @@ def parse_args():
     parser.add_argument("-i", "--import", action="store", dest="import_path",
                         metavar="ISOLINUX/PATH", default=None,
                         help="""import isolinux.cfg into the iso image""")
+    parser.add_argument("-o", "--initrd_overlay", action="store", dest="initrd_overlay",
+                        metavar="INITRD_OVERLAY/PATH", default=None,
+                        help="""inject initrd overlay inside the image""")
 
     opt_args = parser.parse_args()
 
     if opt_args.import_path and not opt_args.dest_iso:
         raise ValueError("The -d/--dest_iso have to be specified when --import/-i is used.")
+
+    if opt_args.initrd_overlay and not opt_args.dest_iso:
+        raise ValueError("The -d/--dest_iso have to be specified when --initrd_overlay/-o is used.")
 
     return opt_args
 
@@ -60,16 +70,77 @@ def import_isolinux_to_iso(iso, dest_iso, isolinux_path):
     if not os.path.isfile(isolinux_path):
         raise ValueError("Provided isolinux.cfg path does not exists!")
 
-    file_name = os.path.basename(isolinux_path)
-
     with TemporaryDirectory(prefix='graft-dir-') as temp_dir:
-        dest_isolinux_path = f"{temp_dir}/isolinux/isolinux.cfg"
-        os.mkdir(f"{temp_dir}/isolinux")
-        copy2(file_name, dest_isolinux_path)
-        # set 444 mod
-        os.chmod(dest_isolinux_path, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
-
+        _copy_isolinux_to_graftdir(isolinux_path, ISOLINUX_PATH, temp_dir)
         create_custom_dvd(iso, dest_iso, temp_dir)
+
+
+def _copy_isolinux_to_graftdir(source_path, dest_path, graft_dir):
+    if not os.path.isdir(graft_dir):
+        raise ValueError(f"Graft directory {graft_dir} does not exists!")
+
+    _make_graft_dirs(graft_dir, dest_path)
+
+    dest_isolinux_path = f"{graft_dir}/{dest_path}"
+
+    _copy_file(source_path, dest_isolinux_path)
+
+
+def _copy_file(source_file, dest_path):
+    """Copy file to with correct permissions for bootloader ISO.
+
+    :param str source_file: file we want to copy
+    :param str dest_path: destination path
+    """
+    copy2(source_file, dest_path)
+    # set 444 mod
+    os.chmod(dest_path, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
+
+
+def _make_graft_dirs(graft_dir, dest_file_path):
+    """Create directories in the graft directory.
+
+    :param str graft_dir: path to the graft directory
+    :param str dest_file_path: path to the file inside of graft directory (relative from graft_dir)
+    """
+    directories, _ = os.path.split(dest_file_path)
+    os.makedirs(f"{graft_dir}/{directories}", exist_ok=True)
+
+
+def inject_initrd_overlay_to_iso(iso, dest_iso, initrd_overlay_path):
+    """Inject initrd overlay to the iso image and change bootloader configuration properly.
+
+    :param str iso: source iso image
+    :param str dest_iso: destination path for the output ISO image
+    :param str initrd_overlay_path: path to the initrd overlay we want to inject
+    """
+    if not os.path.isfile(initrd_overlay_path):
+        raise ValueError("Initrd overlay file does not exists!")
+
+    with TemporaryDirectory(prefix='inject-initrd-graft-dir') as graft_dir:
+        _make_graft_dirs(graft_dir, ISOLINUX_PATH)
+        dest_path = f"{graft_dir}/{ISOLINUX_PATH}"
+        extract_isolinux_from_iso(iso, dest_path)
+        _copy_file(initrd_overlay_path, f"{graft_dir}/isolinux/{INITRD_NAME}")
+
+        _add_overlay_to_isolinux_config(dest_path, INITRD_NAME)
+
+        create_custom_dvd(iso, dest_iso, graft_dir)
+
+
+def _add_overlay_to_isolinux_config(isolinux_path, bootloader_initrd_path):
+    """Inject initrd to the isolinux configuration file."""
+    content = ""
+
+    with open(isolinux_path, 'rt') as f:
+        content = f.read()
+
+    content = content.replace("append initrd=initrd.img",
+                              f"append initrd=initrd.img,{bootloader_initrd_path}")
+
+    with open(isolinux_path, 'wt') as f:
+        f.write(content)
+
 
 if __name__ == "__main__":
     opt = parse_args()
@@ -78,3 +149,5 @@ if __name__ == "__main__":
         extract_isolinux_from_iso(opt.iso, opt.extract_path)
     if opt.import_path and opt.dest_iso:
         import_isolinux_to_iso(opt.iso, opt.dest_iso, opt.import_path)
+    if opt.initrd_overlay:
+        inject_initrd_overlay_to_iso(opt.iso, opt.dest_iso, opt.initrd_overlay)
