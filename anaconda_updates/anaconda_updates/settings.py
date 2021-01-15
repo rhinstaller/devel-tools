f09f90ead338708d3936ca3af558ee4f43a2eec3
Vendula Poncova <vponcova@redhat.com>
2021-01-15 23:39:16 +0100

    Add support for a specific directory
    Use the -d option to specify a path to the anaconda repository.

--- anaconda_updates/anaconda_updates/settings.py
+++ anaconda_updates/anaconda_updates/settings.py
@@ -10,7 +10,7 @@ class GlobalSettings(object):
     #######################
     # Global configuration
     projects_path = ""    # Directory where anaconda blivet and pykickstart are
-    anaconda_path = ""    # Name of the directory with anaconda source codes
+    anaconda_path = ""    # Directory with anaconda source codes
     server = None         # Server with PXE which you are using to test anaconda; could be None
     server_path = "~"     # Server path for saving updates image; default '~'
     local_path = "/tmp"   # Local path to the image; default "/tmp"
@@ -43,17 +43,9 @@ class GlobalSettings(object):
             global_settings = config["GlobalSettings"]
             cls.projects_path = global_settings["ProjectsPath"] # must be specified in config file
             cls.show_version_script_path = global_settings["ShowVersionScriptPath"]
-            cls.anaconda_path = global_settings.get("anaconda_pathName", "anaconda")
-
+            cls.anaconda_path = os.path.join(
+                cls.projects_path, global_settings.get("anaconda_pathName", "anaconda")
+            )
             cls.server = global_settings.get("Server", None)
             cls.server_path = global_settings.get("ServerPath", "~")
             cls.local_path = os.path.expanduser(global_settings.get("LocalPath", "/tmp"))
-
-            # prevent cyclic imports
-            from update_image import DirectoryNotFoundError
-
-            if not os.path.isdir(cls.projects_path):
-                raise DirectoryNotFoundError("Can't find directory {}".format(cls.projects_path))
-            if not os.path.isdir(os.path.join(cls.projects_path, cls.anaconda_path)):
-                raise DirectoryNotFoundError("Can't find directory {}".format(
-                        os.path.join(cls.projects_path, cls.anaconda_path)))
